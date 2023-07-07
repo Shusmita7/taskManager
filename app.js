32399dade8e8ad343b43b57cc8f28ad4bc746e51
Shusmita Das Gupta <shusmitadasgupta@gmail.com>
2023-07-07 21:46:48 +0600

    I create a status option for the tasks and change the design of the pages

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 require('dotenv').config();
 const app = express();
+const moment = require('moment');
 
 
 var con = require("./connection");
 const bodyParser= require("body-parser");
 var session=require('express-session');
+
+let tasks = [];
 
 app.use(function (req,res,next){
     res.set('Cache-Control','no-cache,private,must-revalidate,no-store');
@@ -24,9 +27,6 @@ app.use(express.static('public'));
 
  //route
 
-// app.get("/", function(req,res){
-//     res.render("display.ejs");
-// });
 app.get("/",function (req,res){
     res.render("login.ejs");
  });
@@ -92,78 +92,116 @@ app.get("/display",function (req,res){
     res.render("display.ejs")
 });
 */ 
-app.post("/task", function(req,res){
-    var task = req.body.task;
 
-    console.log(task);
+app.get("/taskAdd",function (req,res){
+    res.render("taskAdd.ejs")
+});
+app.post("/taskAdd",function(req,res){
+    
+    var task= req.body.task;
+    var time=req.body.time;
+    var status='';
+    tasks.push(task);
 
-    var sql = 'INSERT into task (Task) values ?';
+    const now = moment();
+    if (moment(time).isAfter(now)) {
+        status = 'Upcoming';
+    } else if (moment(time).isSameOrBefore(now)) {
+        status = 'Pending';
+  }
 
-    var values =[[task]];
 
-    con.query(sql,[values],function(err,result){
-        if (err) throw err;
-        console.log("Data Uploaded");
+    console.log(task,time);
+    var sql = "INSERT into tasks (task,time,status) values ('"+task+"','"+time+"','"+status+"')";
+    console.log(sql);
 
+    con.query(sql,[task, time, status],function(err){
+        if(err){
+            throw err;
+        }
+        console.log("Data Uploaded Successfully");
         res.redirect("/task");
     })
-})
+
+});
 
 app.get("/task",function(req,res){
-    var sql = 'SELECT * from task';
+    const now = moment();
+    var sql = 'SELECT *, CASE ' +
+            'WHEN Time > now() THEN "Upcoming" ' +
+            'WHEN Time <= now() THEN "Pending" ' +
+            'ELSE "Completed" ' +
+            'END AS Status ' +
+            'FROM tasks';
 
     con.query(sql,function(error,result){
         if(error) throw error;
         
-        res.render("task", { task : result});
+        
+        res.render("task", { tasks: result });
     });
 });
 
-app.get("/updateTask",function(req,res){
-    con.connect(function(error){
-        if(error) console.log(error);
+app.post("/task/:id/complete", function(req, res) {
+    var id = req.params.id;
+  
+    var sql = 'UPDATE tasks SET status = "Complete" WHERE id = ?';
+  
+    con.query(sql, [id], function(err, result) {
+      if (err) {
+        console.log(err);
+        res.redirect("/task");
+      } else {
+        console.log("Task marked as completed");
+        res.redirect("/task");
+      }
+    });
+  });
 
-        var sql = 'SELECT * from task where id = ?';
-
+app.get("/updatetaskData",function(req,res){
         var id = req.query.id;
+        var sql = 'SELECT * from tasks where id = ?';
+
+        
 
         con.query(sql,[id],function(error,result){
             if(error) console.log(error);
 
-            res.render('updateTask',{task: result});
+            res.render('updateTask',{tasks: result});
         })
-    })
-})
 
-app.post('/updateTaskData',function(req,res){
+});
+
+app.post("/updateTaskData",function(req,res){
     var task= req.body.task;
+    var time =req.body.time;
     var id = req.body.id;
 
-    console.log(task,id);
+    console.log(task,time);
 
-    var sql = 'UPDATE task set Task=? where id=?';
+    var sql = 'UPDATE tasks set task=?, time=? where id=?';
 
-    con.query(sql,[task,id],function(err,result){
+    con.query(sql,[task,time,id],function(err,result){
         if(err) console.log(err);
         console.log("Data updated");
         res.redirect("/task");
     })
-})
+});
 
 app.get("/delete",function(req,res){
-    con.connect(function(err){
-        if(err) console.log(err);
+    
         
-        var sql = "DELETE from task where id=?";
-
         var id = req.query.id;
+        
+        var sql = "DELETE from tasks where id=?";
+
 
         con.query(sql,[id],function(error,result){
             if(error) console.log(error);
             res.redirect("/task");
         })
-    })
-})
+
+});
 
 app.get("/logout",function (req,res){
     req.session.destroy((error)=>{
